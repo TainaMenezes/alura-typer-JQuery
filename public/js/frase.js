457465f3c4853359b30bc9bb6092af78f05578ed
TainaMenezes <tainamenezescris@gmail.com>
2021-04-12 18:02:42 -0300

    Curso JQuery Parte 2. Animações.

---
+++ public/js/frase.js
@@ -0,0 +1,23 @@
+$("#botao-frase").click(fraseAleatoria);
+
+function fraseAleatoria() {
+  $("#spinner").toggle();
+
+  $.get("http://localhost:3000/frases", trocaFraseAleatoria).fail(function() {
+    $("#erro").toggle();
+    setTimeout(function() {
+      $("#erro").toggle();
+    },2000)
+  }).always(function() {
+    $("#spinner").toggle();
+  });
+}
+
+function trocaFraseAleatoria(data) {
+  var frase = $(".frase");
+  var numAleatorio = Math.floor(Math.random() * data.length);
+  frase.text(data[numAleatorio].texto);
+
+  atualizaTamanhoFrase();
+  atualizaTempoInicial(data[numAleatorio].tempo);
+}
